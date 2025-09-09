300676599e68086773b429e28e4705f9277d0a77
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2025-09-08 22:13:27 -0700

    Build AI powered apps certificate

--- src/routes/certification/certification.component.jsx
+++ src/routes/certification/certification.component.jsx
@@ -1,48 +1,50 @@
-import Backend from "../../assets/backend.png";
-import HeadingOnePrimary from "../../core-components/headings/heading-one-primary/heading-one-primary.component";
-import "./certification.styles.scss";
-import { Link, useLocation } from "react-router-dom";
-import PythonCert from "../../assets/certs/certificate-of-completion-for-python.png";
-import HTMLCSSCert from "../../assets/certs/The Ultimate HTML5 And CSS3_Part1.jpg";
-import DSACert from "../../assets/certs/UC-3BSD8H27_DSA.jpg";
-import TypescriptCert from "../../assets/certs/1-the-ultimate-typescript-course.png";
-import ButtonPrimary from "../../core-components/buttons/button-primary/button-primary.component";
-import { useNavigate } from "react-router-dom";
+import Backend from '../../assets/backend.png'
+import HeadingOnePrimary from '../../core-components/headings/heading-one-primary/heading-one-primary.component'
+import './certification.styles.scss'
+import { Link, useLocation } from 'react-router-dom'
+import PythonCert from '../../assets/certs/certificate-of-completion-for-python.png'
+import HTMLCSSCert from '../../assets/certs/The Ultimate HTML5 And CSS3_Part1.jpg'
+import DSACert from '../../assets/certs/UC-3BSD8H27_DSA.jpg'
+import TypescriptCert from '../../assets/certs/1-the-ultimate-typescript-course.png'
+import BuildAIPoweredApps from '../../assets/certs/certificate-of-completion-for-build-ai-powered-apps.png'
+import ButtonPrimary from '../../core-components/buttons/button-primary/button-primary.component'
+import { useNavigate } from 'react-router-dom'
 
 const Certification = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const certs = {
     python: PythonCert,
     htmlcss: HTMLCSSCert,
     dsa: DSACert,
-    typescript: TypescriptCert
-  };
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const cert = queryParams.get("cert");
+    typescript: TypescriptCert,
+    buildAIPoweredApps: BuildAIPoweredApps
+  }
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const cert = queryParams.get('cert')
   return (
     <div className="certification-container">
       <div className="certbtn-group-container">
         {Object.keys(certs).map((cert) => {
-          console.log(cert);
+          console.log(cert)
           // const certVal = certs[cert];
           return (
             <Link
               className="cert-nav-link"
               key={cert}
               onClick={(event) => {
-                event.preventDefault();
-                navigate(`/certification?cert=${cert}`);
+                event.preventDefault()
+                navigate(`/certification?cert=${cert}`)
               }}
             >
               {cert}
             </Link>
-          );
+          )
         })}
       </div>
       <img className="certimage-container" src={certs[cert]} alt={cert} />
     </div>
-  );
-};
+  )
+}
 
-export default Certification;
+export default Certification
